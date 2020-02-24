97744966cc24ffd4a2dc3c3ab2e00828d10a1fa3
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-24 01:45:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,16 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :material, :color, :size
+  attr_accessor :material, :color, :size, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
-  def condition
-    puts "Your shoe is as good as new!"
+  def condition= (condition)
+    @condition = condition
   end
+  def condition
 end
 
 Shoe.new("Nike")
