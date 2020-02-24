f456af15720b832f4578207554ab6b462937b396
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-24 01:37:07 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,6 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  def initialize(title)
-    @title = title
-  end
-  def title
-    @title
-  end
+  attr_reader :title
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
